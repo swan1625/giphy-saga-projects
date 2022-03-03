3331a617bda0d604df3f3ed01d8b2c79417a1c55
Clay Webb <clay_webb@outlook.com>
2022-03-03 14:55:07 -0600

    Updated favoritesList

--- src/reducers/favoritesList.reducer.js
+++ src/reducers/favoritesList.reducer.js
@@ -1,6 +1,7 @@
 import {put} from 'redux-saga/effects';
 import axios from 'axios';
 
+
 const favoritesList = (state = [], action) => {
     if (action.type === 'MAKE_FAVORITE') {
         return [...state, action.payload]
