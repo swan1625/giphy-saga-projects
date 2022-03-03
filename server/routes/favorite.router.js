18935083265868ed8f3eb7e8af98b71bf79fc320
Clay Webb <clay_webb@outlook.com>
2022-03-03 13:52:47 -0600

    Successful post in favorite.router.js

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,12 +5,26 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
+  pool.query(`SELECT * FROM "favorites"."id"  `)
   res.sendStatus(200);
 });
 
 // add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  let newFavorite = req.body.url;
+  console.log('In favorite.router.js and the req.body is:', newFavorite);
+  const sqlQuery = `INSERT INTO "favorites"
+  (url)
+  VALUES ($1);`;
+  pool.query(sqlQuery, [newFavorite])
+  .then((response) => {
+    console.log('Successful Post in favorite.router.js!!!!', response);
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log(error);
+    res.sendStatus(500);
+  })
+  
 });
 
 // update given favorite with a category id
