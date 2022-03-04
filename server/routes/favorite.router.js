25f699da803e94f2de9f26bf710d8f9a22bb93a3
Clay Webb <clay_webb@outlook.com>
2022-03-04 01:07:33 -0600

    Updated router.post

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  let queryText = 'SELECT * FROM favorites;';
+  let queryText = 'SELECT favorites.id, favorites.url, favorites.category_id, category.name FROM favorites JOIN category ON favorites.category_id = category.id;';
   pool.query(queryText)
   .then((result) => {
+    // console.log('Success!');
     res.send(result.rows);
   }).catch((err) => {
     console.log('Error in get on favorite.router.js!!', err);
@@ -17,14 +18,16 @@ router.get('/', (req, res) => {
 
 // add a new favorite
 router.post('/', (req, res) => {
-  let newFavorite = req.body.url;
-  console.log('In favorite.router.js and the req.body is:', newFavorite);
-  const sqlQuery = `INSERT INTO "favorites"
-  (url)
-  VALUES ($1);`;
-  pool.query(sqlQuery, [newFavorite])
+  let newFav = req.body;
+  console.log('In favorite.router.js and the req.body is:', newFav);
+  let sqlQuery = `INSERT INTO "favorites" ("url")
+                    VALUES ($1);`;
+  // pool.query(queryText, [newFavorite.url, newFavorite.category_id])
+  pool.query(sqlQuery, [newFav.url])
+
   .then((response) => {
-    console.log('Successful Post in favorite.router.js!!!!', response);
+    console.log('Successful Post!!', response);
+    
     res.sendStatus(200);
   }).catch((error) => {
     console.log(error);
